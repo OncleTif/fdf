94ea3f3647408572cb4893890144996a489e5a8e
tmanet <tmanet@student.42.fr>
2016-02-11 18:41:43 +0100

    ajout de la lib

---
+++ libft/ft_atoi_strict.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_strict.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/08 16:30:05 by tmanet            #+#    #+#             */
+/*   Updated: 2016/02/10 17:34:12 by tmanet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi_strict(const char *str)
+{
+	int		i;
+	long	nbr;
+	int		neg;
+
+	i = 0;
+	nbr = 0;
+	neg = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		neg = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			ft_error("invalid input in atoi_strict");
+		i++;
+	}
+	if (str[i])
+		ft_error("invalid input in atoi_strict");
+	return ((int)nbr * neg);
+}
