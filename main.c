079d7f23c32a4102dd0427b29a4603a82e9cd0b0
tmanet <tmanet@student.42.fr>
2016-02-12 12:22:44 +0100

    protection des fichiers vides

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 11:57:48 by tmanet            #+#    #+#             */
-/*   Updated: 2016/02/04 15:43:45 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/02/12 12:19:44 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	main(int argc, char **argv)
 {
 	t_list	*list;
 
-	if (argc > 1 && argv)
+	if (argc == 2 && argv)
 	{
 		list = ft_reader(argv[1]);
 		make_window(list);
 	}
-	ft_error("not enough args");
+	ft_error("error in args");
 	return (0);
 }
--- make_window.c
+++ make_window.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 13:15:12 by tmanet            #+#    #+#             */
-/*   Updated: 2016/02/11 17:22:02 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/02/12 12:22:07 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	make_window(t_list *list)
 	graph->x_offset = WIDTH / 2;
 	graph->y_offset = HEIGHT / 2;
 	ft_map_builder(list, graph);
+	if (!graph->x_max)
+		ft_error("file does not contain a map");
 	ft_zoom_opti(graph);
 	graph->keymap = ft_key_mapping();
 	graph->mlx = mlx_init();
